ce6bc07c018b7a2bd8449cd238788acbb569790e
Edoardo <ercolino.edoardo@gmail.com>
2021-10-26 16:11:14 +0200

    conclusione + bonus

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+           <h1>I miei contatti</h1>
+           <p>+334455667788</p>
+
+           <a href="/">home</a>
+           <a href="/about">about me</a>
+        </div>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $data=[
+        'nome' => 'edoardo',
+        'cognome' => 'ercolino'
+    ];
+    return view('home', $data);
+});
+
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+
+Route::get('/about', function () {
+    return view('about');
 });
